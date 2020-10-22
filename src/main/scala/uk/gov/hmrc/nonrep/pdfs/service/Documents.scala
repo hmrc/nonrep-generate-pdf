a52b5e94dc38af100db78d8f2e2ff828cf925f48
greg.gajda@digital.hmrc.gov.uk <29655695+greg-gajda@users.noreply.github.com>
2020-10-22 15:53:01 +0100

    NONPR-1683 File system for mocked PDF document

--- src/main/scala/uk/gov/hmrc/nonrep/pdfs/service/Documents.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/pdfs/service/Documents.scala
@@ -1,18 +1,30 @@
 package uk.gov.hmrc.nonrep.pdfs
 package service
 
-import uk.gov.hmrc.nonrep.pdfs.model.{ClientTemplate, GeneratePdfRequest, GeneratePdfResponse, Template}
+import java.nio.file.spi.FileSystemProvider
+import java.nio.file.{FileSystems, Files, Paths}
+
+import uk.gov.hmrc.nonrep.pdfs.model.{AcceptedRequest, GeneratePdfDocument, SignPdfDocument, Template}
 import uk.gov.hmrc.nonrep.pdfs.server.ServiceConfig
+
+import scala.jdk.CollectionConverters._
+import scala.util.Try
 
 object Documents {
   import Converters._
   import HashCalculator._
   import HashCalculator.ops._
 
-  def createPdfDocument(request: GeneratePdfRequest): EitherNelErr[GeneratePdfResponse] =
-    Right(GeneratePdfResponse(request.payload.incomingData.calculateHash(), Array[Byte]()))
+  def createPdfDocument(request: GeneratePdfDocument): EitherNelErr[SignPdfDocument] =
+    Right(SignPdfDocument(request.payload.incomingData.calculateHash(), request.template.profile, Array[Byte]()))
 
-  def findPdfDocumentTemplate(template: ClientTemplate)(implicit config: ServiceConfig): EitherNelErr[Template] =
-    config.templates.get(template.key).flatMap(_.find(_.id == template.id)).toEitherNel(404, s"Unknown template '$template'")
+  def createPdfDocument(request: EitherNelErr[GeneratePdfDocument]): EitherNelErr[SignPdfDocument] = {
+    val uri = getClass.getClassLoader.getResource("2020-06-26-trusts_sample.pdf").toURI
+    Try(FileSystems.newFileSystem(uri, Map[String, Any]().asJava))
+    val doc = Files.readAllBytes(Paths.get(uri))
+    request.map(x => SignPdfDocument(x.payload.incomingData.calculateHash(), x.template.profile, doc))
+  }
 
+  def findPdfDocumentTemplate(request: EitherNelErr[AcceptedRequest])(implicit config: ServiceConfig): EitherNelErr[Template] =
+    request.flatMap(req => config.templates.get(req.key).flatMap(_.find(_.id == req.template)).toEitherNel(404, s"Unknown template '$req.template'"))
 }
